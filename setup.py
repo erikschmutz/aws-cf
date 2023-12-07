2ad0e0a2bed5ab3427df09992423777118985a68
Erik Rehn <erik.rehn@altostruct.se>
2023-12-07 17:43:08 +0100

    added more stuff

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='aws-cf',
-    version='0.0.14',    
+    version='0.0.15',    
     description='Simple way to deploy AWS stacks',
     long_description='Simple way to deploy AWS stacks',
     url='https://github.com/erikschmutz/aws-cf/',
     author='Erik Rehn',
     author_email='erik.rehn98@gmail.com',
     license='BSD 2-clause',
-    packages=['aws_cf'],
+    packages=['aws_cf', 'aws_cf.utils'],
     install_requires=['pydantic', 'boto3'],
     entry_points={
         'console_scripts': [
