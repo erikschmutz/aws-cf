a1893a60fb01fd5f5d844f073580b7593b6706e5
Erik Rehn <erik.rehn@altostruct.se>
2023-12-07 16:58:26 +0100

    added version

--- src/__init__.py
+++ src/__init__.py
@@ -4,11 +4,15 @@ from utils.logging import logger
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("action", choices=['diff', 'deploy', 'package'], help='what action to preform')
+parser.add_argument("action", choices=['diff', 'deploy', 'package', "version"], help='what action to preform')
 parser.add_argument("path", help='path to the file describing the services')
+parser.add_argument("-v", '--version', action='version', help='path to the file describing the services', version='%(prog)s 0.0.0')
 parser.add_argument("-r", "--root")
 
 args = parser.parse_args()
+
+if args.action:
+    print("Version: ")
 
 if args.action == "deploy":
     try:
